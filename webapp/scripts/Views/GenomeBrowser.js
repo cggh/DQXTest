b609d7e41d59427942e3d9588f32cbabe207ca52
pvaut <paul.vauterin@gmail.com>
2013-07-07 08:35:52 +0100

    SNP position indicator channel

--- webapp/scripts/Views/GenomeBrowser.js
+++ webapp/scripts/Views/GenomeBrowser.js
@@ -1,5 +1,5 @@
-define(["require", "DQX/Application", "DQX/Framework", "DQX/HistoryManager", "DQX/Controls", "DQX/Msg", "DQX/DocEl", "DQX/Utils", "DQX/ChannelPlot/GenomePlotter", "DQX/ChannelPlot/ChannelYVals", "DQX/ChannelPlot/ChannelSequence", "DQX/DataFetcher/DataFetchers", "DQX/DataFetcher/DataFetcherSummary", "MetaData"],
-    function (require, Application, Framework, HistoryManager, Controls, Msg, DocEl, DQX, GenomePlotter, ChannelYVals, ChannelSequence, DataFetchers, DataFetcherSummary, MetaData) {
+define(["require", "DQX/Application", "DQX/Framework", "DQX/HistoryManager", "DQX/Controls", "DQX/Msg", "DQX/DocEl", "DQX/Utils", "DQX/ChannelPlot/GenomePlotter", "DQX/ChannelPlot/ChannelYVals", "DQX/ChannelPlot/ChannelPositions", "DQX/ChannelPlot/ChannelSequence","DQX/DataFetcher/DataFetchers", "DQX/DataFetcher/DataFetcherSummary", "MetaData"],
+    function (require, Application, Framework, HistoryManager, Controls, Msg, DocEl, DQX, GenomePlotter, ChannelYVals, ChannelPositions, ChannelSequence, DataFetchers, DataFetcherSummary, MetaData) {
 
         var GenomeBrowserModule = {
 
@@ -26,8 +26,8 @@ define(["require", "DQX/Application", "DQX/Framework", "DQX/HistoryManager", "DQ
                 };
 
 
+                //Create the genome browser panel
                 that.createPanelBrowser = function() {
-
                     //Browser configuration settings
                     var browserConfig = {
                         serverURL: MetaData.serverUrl,              //Url of the DQXServer instance used
@@ -38,6 +38,16 @@ define(["require", "DQX/Application", "DQX/Framework", "DQX/HistoryManager", "DQ
                         viewID: '',
                         canZoomVert: true                           //Viewer contains buttons to alter the vertical size of the channels
                     };
+                    //NOTE: the annotation table should have the following fields:
+                    //    chromid: (varchar) identifier of the chromosome
+                    //    fstart,fstop: (int) start & stop of the feature
+                    //    fid: (varchar) feature identifier
+                    //    ftype: (varchar) feature type identifier, can be 'gene' or 'exon'
+                    //    fparentid: (varchar) in case of exon, identifier of the feature it belongs to
+                    //    fname: (varchar) display name of the feature
+                    //    fnames: (varchar) ;-separated list of alternative names (may be empty)
+                    //    descr: (varchar) description of the gene (may be empty)
+
                     //Initialise a genome browser panel
                     this.panelBrowser = GenomePlotter.Panel(this.frameBrowser, browserConfig);
 
@@ -51,15 +61,11 @@ define(["require", "DQX/Application", "DQX/Framework", "DQX/HistoryManager", "DQ
                         alert('Clicked on gene '+geneID);
                     }
 
-
-                    that.createFrequencyChannels();
-                    that.createSummaryChannels();
-                };
-
-
-                //Creates the channel in the browser that displays allele frequencies
-                that.createFrequencyChannels = function() {
                     //Create the data fetcher that will get the frequency values from the server
+                    //NOTE: the values table should have the following fields:
+                    //        chrom: (int) index number of the chromosome (1-based) (note: this can be changed to chromosome id strings by changing the browser configuration
+                    //        pos: (int) position in bp on the chromosome (1-based)
+                    //        various values to be plotted (float)
                     this.dataFetcherSNPs = new DataFetchers.Curve(
                         MetaData.serverUrl,     //url of the DQXServer instance providing the data
                         MetaData.database,      //name of the database
@@ -68,6 +74,31 @@ define(["require", "DQX/Application", "DQX/Framework", "DQX/HistoryManager", "DQ
 
                     //add snpid column to the datafetcher, not plotted but needed for the tooltip & click actions
                     this.dataFetcherSNPs.addFetchColumnActive("snpid", "String");
+
+                    //Fill with channels
+                    that.createSnpPositionChannel();
+                    that.createFrequencyChannels();
+                    that.createSummaryChannels();
+                };
+
+                //Creates a channel that shows the SNP positions
+                that.createSnpPositionChannel = function() {
+                    var theChannel = ChannelPositions.Channel(null,
+                        that.dataFetcherSNPs,   // The datafetcher containing the positions of the snps
+                        'snpid'                 // Name of the column containing a unique identifier for each snp
+                    );
+                    theChannel
+                        .setTitle("SNP positions")        //sets the title of the channel
+                        .setMaxViewportSizeX(5.0e5);     //if more than 5e5 bases are in the viewport, this channel is not shown
+                    theChannel.makeCategoricalColors(//Assign a different color to silent/nonsilent snps
+                        'MutType',               // Name of the column containing a categorical string value that determines the color of the snp
+                        { 'S' :  DQX.Color(1,1,0) , 'N' : DQX.Color(1,0.4,0) }   //Map of value-color pairs
+                    );
+                    that.panelBrowser.addChannel(theChannel, false);//Add the channel to the browser
+                }
+
+                //Creates the channel in the browser that displays allele frequencies
+                that.createFrequencyChannels = function() {
 
                     //Create the channel in the browser that will contain the frequency values
                     var theChannel = ChannelYVals.Channel(null, { minVal: 0, maxVal: 1 });
@@ -126,15 +157,23 @@ define(["require", "DQX/Application", "DQX/Framework", "DQX/HistoryManager", "DQ
                         //Create the min-max range
                         var colinfo_min = that.dataFetcherProfiles.addFetchColumn(folder, MetaData.summaryConfig, profile.id + "_min");//get the min value from the fetcher
                         var colinfo_max = that.dataFetcherProfiles.addFetchColumn(folder, MetaData.summaryConfig, profile.id + "_max");//get the max value from the fetcher
-                        SummChannel.addComponent(ChannelYVals.YRange(null, that.dataFetcherProfiles, colinfo_min.myID, colinfo_max.myID, DQX.Color(0.3, 0.3, 0.7, 0.35)), true);//Define the range component
+                        SummChannel.addComponent(ChannelYVals.YRange(null,//Define the range component
+                            that.dataFetcherProfiles,               // data fetcher containing the profile information
+                            colinfo_min.myID,                       // fetcher column id for the min value
+                            colinfo_max.myID,                       // fetcher column id for the max value
+                            DQX.Color(0.3, 0.3, 0.7, 0.35)          // color of the range
+                        ), true );
 
                         //Create the average value profile
                         var colinfo_avg = that.dataFetcherProfiles.addFetchColumn(folder, MetaData.summaryConfig, profile.id + "_avg");//get the avg value from the fetcher
-                        var comp = SummChannel.addComponent(ChannelYVals.Comp(null, that.dataFetcherProfiles, colinfo_avg.myID), true);//Add the profile to the channel
-                        comp.setColor(DQX.Color(0, 0, 0.5));
+                        var comp = SummChannel.addComponent(ChannelYVals.Comp(null,//Add the profile to the channel
+                            that.dataFetcherProfiles,               // data fetcher containing the profile information
+                            colinfo_avg.myID                        // fetcher column id containing the average profile
+                        ), true);
+                        comp.setColor(DQX.Color(0, 0, 0.5));//set the color of the profile
                         comp.myPlotHints.makeDrawLines(3000000.0); //that causes the points to be connected with lines
                         comp.myPlotHints.interruptLineAtAbsent = true;
-                        comp.myPlotHints.drawPoints = false;
+                        comp.myPlotHints.drawPoints = false;//only draw lines, no individual points
 
                     })
 
