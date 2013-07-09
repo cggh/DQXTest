f896f7b40f96fcd612e4a4379932d5d5353833ff
Paul Vauterin <paul.vauterin@gmail.com>
2013-07-09 12:55:51 +0100

    Automatic creation of advanced query panel

--- webapp/scripts/Views/TableViewer.js
+++ webapp/scripts/Views/TableViewer.js
@@ -41,8 +41,12 @@ define(["require", "DQX/Application", "DQX/Framework", "DQX/HistoryManager", "DQ
                 //This function is called during the initialisation. Create the frame structure of the view here
                 that.createFrames = function(rootFrame) {
                     rootFrame.makeGroupHor();//Declare the root frame as a horizontally divided set of subframes
-                    this.frameControls = rootFrame.addMemberFrame(Framework.FrameFinal('', 0.3));//Create frame that will contain the controls panel
-                    this.frameTable = rootFrame.addMemberFrame(Framework.FrameFinal('', 0.7))//Create frame that will contain the table viewer
+                    this.frameQueriesContainer = rootFrame.addMemberFrame(Framework.FrameGroupTab('', 0.4));//Create frame that will contain the query panels
+                    this.frameQueryAdvanced = this.frameQueriesContainer.addMemberFrame(Framework.FrameFinal(''))
+                        .setDisplayTitle('Advanced query');//Create frame that will contain the query panels
+                    this.frameQuerySimple = this.frameQueriesContainer.addMemberFrame(Framework.FrameFinal(''))
+                        .setDisplayTitle('Simple query');//Create frame that will contain the query panels
+                    this.frameTable = rootFrame.addMemberFrame(Framework.FrameFinal('', 0.6))//Create frame that will contain the table viewer
                         .setAllowScrollBars(false,true);
                 }
 
@@ -50,10 +54,6 @@ define(["require", "DQX/Application", "DQX/Framework", "DQX/HistoryManager", "DQ
 
                 //This function is called during the initialisation. Create the panels that will populate the frames here
                 that.createPanels = function() {
-                    this.createPanelTableViewer();
-                };
-
-                that.createPanelTableViewer = function () {
 
                     //Initialise the data fetcher that will download the data for the table
                     this.theTableFetcher = DataFetchers.Table(
@@ -62,6 +62,25 @@ define(["require", "DQX/Application", "DQX/Framework", "DQX/HistoryManager", "DQ
                         MetaData.tableSNPInfo   //name of the table containing the data
                     );
                     this.theTableFetcher.showDownload=true; //Allows the user to download the data in the table
+
+                    this.createPanelTableViewer();
+
+                    // Define an "advanced query" panel
+                    this.panelTable.createPanelAdvancedQuery(this.frameQueryAdvanced);
+
+                    // Create the "simple query" panel
+                    this.createPanelSimpleQuery();
+
+                    //Make sure that the query results are reset each time another type of query is chosen
+                    Msg.listen('',{ type: 'ChangeTab', id: this.frameQueriesContainer.getFrameID() }, function() {
+                        that.panelTable.invalidateQuery();
+                    });
+
+
+                };
+
+                //Create the table viwer panel
+                that.createPanelTableViewer = function () {
                     //Initialise the panel that will contain the table
                     this.panelTable = QueryTable.Panel(
                         this.frameTable,        // Frame this panel should be located in
@@ -70,36 +89,43 @@ define(["require", "DQX/Application", "DQX/Framework", "DQX/HistoryManager", "DQ
                             leftfraction: 50    // Max size (in %) of the left, non-scrolling component of the table
                         }
                     );
+                    this.myTable = this.panelTable.getTable();// A shortcut variable
 
-                    var mytable = this.panelTable.getTable();// A shortcut variable
+                    // Add a column for chromosome
+                    var comp = that.myTable.createTableColumn(
+                        QueryTable.Column(
+                            "Chrom.",       // Display name of the column
+                            "chrom",        // ID (=Column name in the database table
+                            0),             // Table part (1=right, scrolling)
+                        "Int",              // Transfer encoding: integer (see DataFetchers.CurveColumn for possible choices)
+                        false               // Column is not sortable by itself (sorted via joint statement chrom+pos)
+                    );
+                    comp.CellToText = function(nr) { return MetaData.chromosomes[nr-1].id; }; // Converts the number to a chromosome name
 
-                    // Add a column with the chromosome nr & position
-                    // NOTE: The follow code contains some specialised actions, because it combines both the chromosome number & position in a single table column
-                    this.theTableFetcher.addFetchColumnInt("chrom"); // Fetch the chromosome, but don't make a table column for it
-                    var comp = mytable.createTableColumn(QueryTable.Column("Position", "pos", 0), "IntB64", false); // Create a table column for the position
-                    comp.customTextCreator = function(fetcher,downloadrownr) { // Define a custom function for creating the text of this column, combining both data sets
-                        return fetcher.getColumnPoint(downloadrownr, 'chrom')+':'+fetcher.getColumnPoint(downloadrownr, 'pos');
-                    }
-                    comp.setToolTip('Chromosome number and position on the chromosome, in base pairs'); // Define a hover tooltip
-                    mytable.addSortOption("Position", SQL.TableSort(['chrom', 'pos'])); // Define a joint sort action on both columns
+                    // For the query tools, define this column as a multiple choice set
+                    var chromPickList = [];
+                    $.each(MetaData.chromosomes,function(idx,chrom) {
+                        chromPickList.push({ id: idx+1, name: MetaData.chromosomes[idx].id });
+                    })
+                    comp.setDataType_MultipleChoiceInt(chromPickList);
+
+                    // Add a column for position
+                    var comp = that.myTable.createTableColumn(QueryTable.Column("Position.","pos",0),"IntB64",false);
+                    that.myTable.addSortOption("Position", SQL.TableSort(['chrom', 'pos'])); // Define a joint sort action on both columns chrom+pos
 
 
                     // Add a column for the SNP identifier
                     // NOTE: this is a more standard way of adding a table column
-                    var comp = mytable.createTableColumn(
-                        QueryTable.Column(
-                            "SNP ID",       // Display name of the column
-                            "snpid",        // ID (=Column name in the database table
-                            1),             //Table part (1=right, scrolling)
-                        "String",           //Transfer encoding: general string (see DataFetchers.CurveColumn for possible choices)
-                        true                // Column is sortable
-                    );
+                    var comp = that.myTable.createTableColumn(QueryTable.Column("SNP ID","snpid",1),"String",true);
                     comp.setToolTip('SNP identifier');  // Hover tooltip
-
+                    // Define a handler that will be called when a user clicks this column (note: this will turn this field into a hyperlink)
+                    comp.setCellClickHandler(function(fetcher,downloadrownr) {
+                        alert('Clicked on snp '+fetcher.getColumnPoint(downloadrownr, 'snpid'));
+                    })
 
                     //Create a column for each population frequency
                     $.each(MetaData.populations,function(idx,pop) {
-                        var col = mytable.createTableColumn(
+                        var col = that.myTable.createTableColumn(
                             QueryTable.Column(
                                 pop.freqid,       //Name of the column
                                 pop.freqid,       //Id of the column in the database table
@@ -116,12 +142,22 @@ define(["require", "DQX/Application", "DQX/Framework", "DQX/HistoryManager", "DQ
                         col.CellToColor = funcFraction2Color; //Create a background color that reflects the value
                     })
 
-
+                    //Add some  more columns
+                    that.myTable.createTableColumn(QueryTable.Column("Gene names","aliases",1),"String",true);
+                    that.myTable.createTableColumn(QueryTable.Column("Gene description","GeneDescription",1),"String",true);
 
                     //we start by defining a query that returns everything
-                    mytable.queryAll();
+                    that.myTable.queryAll();
 
                 };
+
+
+                that.createPanelSimpleQuery = function () {
+                    this.panelSimpleQuery = Framework.Form(this.frameQuerySimple);
+                    this.panelSimpleQuery.setPadding(10);
+                    var theForm=this.panelSimpleQuery;
+
+                }
 
 
 
